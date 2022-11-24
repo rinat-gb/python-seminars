56f8545f23a8cf363264adb428ffe3ba56708555
Rinat H. Sadretdinow <rinsdr@yandex.ru>
2022-11-24 20:14:07 +0300

    Unnessesary variable 'result' removed

--- homework_lesson_1.py
+++ homework_lesson_1.py
@@ -39,7 +39,6 @@
 #     y = int(binary_string[1])
 #     z = int(binary_string[2])
 #
-#     result = not (x or y or z) == (not x) and (not y) and (not z)
 #     print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} есть {not (x or y or z) == (not x) and (not y) and (not z)}')
 #
 
