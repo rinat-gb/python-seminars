4002067b94760f2939e7e5df2c04109777b4c981
Rinat H. Sadretdinow <rinsdr@yandex.ru>
2022-11-24 20:44:10 +0300

    Added forgotten comments

--- homework_lesson_1.py
+++ homework_lesson_1.py
@@ -34,12 +34,11 @@
 # print('Проверка истиности выражения "¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z" для всех возможных значений предикат:\n')
 #
 # for i in range(0b111 + 1):
-#     binary_string = format(i, '03b')
-#     x = int(binary_string[0])
-#     y = int(binary_string[1])
-#     z = int(binary_string[2])
-#
-#     print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} есть {not (x or y or z) == (not x) and (not y) and (not z)}')
+#    binary_string = format(i, '03b')
+#    x = int(binary_string[0])
+#    y = int(binary_string[1])
+#    z = int(binary_string[2])
+#    print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} есть {not (x or y or z) == (not x) and (not y) and (not z)}')
 #
 
 ######################################################################
