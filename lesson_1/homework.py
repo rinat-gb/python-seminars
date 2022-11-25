0f1524f2923cbf1bea02854852b7022c0702d2f2
Rinat H. Sadretdinow <rinsdr@yandex.ru>
2022-11-25 18:03:48 +0300

    lesson_1/homework.py added due to new directory tree model

--- lesson_1/homework.py
+++ lesson_1/homework.py
@@ -35,9 +35,11 @@
 #
 # for i in range(0b111 + 1):
 #    binary_string = format(i, '03b')
+#
 #    x = int(binary_string[0])
 #    y = int(binary_string[1])
 #    z = int(binary_string[2])
+#
 #    print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} есть {not (x or y or z) == (not x) and (not y) and (not z)}')
 #
 
